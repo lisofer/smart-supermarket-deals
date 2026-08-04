d6244aa8a3a7b2cf12cc74c277cda4cc8591a2ad
lisofer <60150165+lisofer@users.noreply.github.com>
2026-08-04 00:57:01 -0300

    Enable parser unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    testImplementation("junit:junit:4.13.2")
 }
 
 kotlin {
