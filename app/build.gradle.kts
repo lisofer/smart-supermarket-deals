837782a2effb97f993799d16e30ec9b919d8bcb6
lisofer <60150165+lisofer@users.noreply.github.com>
2026-08-04 09:59:25 -0300

    Bump version for search endpoint experiment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lisofer.smartsupermarketdeals"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "1.1.0"
     }
 
     buildTypes {
