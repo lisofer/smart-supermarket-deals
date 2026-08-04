9bed63edc195c9df1126d61b7cb162d37b57fa3a
lisofer <60150165+lisofer@users.noreply.github.com>
2026-08-03 21:57:38 -0300

    chore: bump app version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lisofer.smartsupermarketdeals"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildTypes {
