855e07c8c81d4830cb36a7519c837e9278cc4376
lisofer <60150165+lisofer@users.noreply.github.com>
2026-08-04 01:33:20 -0300

    Bump Android app to v0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lisofer.smartsupermarketdeals"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.8.0"
+        versionCode = 9
+        versionName = "0.9.0"
     }
 
     buildTypes {
