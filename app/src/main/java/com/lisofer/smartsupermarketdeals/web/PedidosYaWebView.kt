04479d9e41563d26f30b832ebc17c862947cdf35
lisofer <60150165+lisofer@users.noreply.github.com>
2026-08-04 09:58:45 -0300

    Inject search endpoint catalog harvester

--- app/src/main/java/com/lisofer/smartsupermarketdeals/web/PedidosYaWebView.kt
+++ app/src/main/java/com/lisofer/smartsupermarketdeals/web/PedidosYaWebView.kt
@@ -193,6 +193,11 @@ fun PedidosYaWebView(
                             exhaustiveCatalogScript,
                             allowedOrigins,
                         )
+                        WebViewCompat.addDocumentStartJavaScript(
+                            this,
+                            searchCatalogHarvesterScript,
+                            allowedOrigins,
+                        )
                     } else {
                         currentOnUnsupported.value()
                     }
