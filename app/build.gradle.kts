a7d7dfc7a2beed2686077f52ca8276c35491ddb7
lisofer <60150165+lisofer@users.noreply.github.com>
2026-08-05 19:21:52 -0300

    Bump version to 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lisofer.smartsupermarketdeals"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.2.4"
+        versionCode = 17
+        versionName = "1.2.5"
     }
 
     buildTypes {
